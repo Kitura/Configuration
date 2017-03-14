967c2952be33b9bbd54a09b944d07d5ddbfb564b
Youming Lin <youminglin@outlook.com>
2017-03-14 09:17:49 -0500

    Clean up code

--- Sources/Configuration/Utilities.swift
+++ Sources/Configuration/Utilities.swift
@@ -16,55 +16,59 @@
 
 import Foundation
 
-func executableFolderURL() -> URL {
-#if os(Linux)
-    let actualExecutableURL = Bundle.main.executableURL
-                              ?? URL(fileURLWithPath: "/proc/self/exe").resolvingSymlinksInPath()
-    return actualExecutableURL.appendingPathComponent("..").standardized
-#else
-    let actualExecutableURL = Bundle.main.executableURL
-                              ?? URL(fileURLWithPath: CommandLine.arguments[0]).standardized
-    let actualExecutableFolderURL = actualExecutableURL.appendingPathComponent("..").standardized
+let executableFolderURL = { () -> URL in
+    #if os(Linux)
+        // Bundle is not available on Linux yet
+        // Get path to exe via /proc/self/exe
+        // https://unix.stackexchange.com/questions/333225/which-process-is-proc-self-for
+        return URL(fileURLWithPath: "/proc/self/exe").resolvingSymlinksInPath().appendingPathComponent("..")
+    #else
+        // Bundle is available on Darwin
+        let actualExecutableURL = Bundle.main.executableURL ?? URL(fileURLWithPath: CommandLine.arguments[0])
+        let actualExecutableFolderURL = actualExecutableURL.appendingPathComponent("..")
 
-    if (Bundle.main.executableURL?.lastPathComponent != "xctest") {
-        return actualExecutableFolderURL
-    } else {
-        // We are running under the test runner, we may be able to work out the build directory that
-        // contains the test program which is testing libraries in the project. That build directory
-        // should also contain any executables associated with the project until this build type
-        // (eg: release or debug)
-        let loadedTestBundles = Bundle.allBundles.filter({ $0.isLoaded }).filter({ $0.bundlePath.hasSuffix(".xctest") })
-        if loadedTestBundles.count > 0 {
-            return loadedTestBundles[0].bundleURL.appendingPathComponent("..").standardized
-        } else {
+        if (actualExecutableURL.lastPathComponent != "xctest") {
             return actualExecutableFolderURL
         }
-    }
-#endif
-}
+        else {
+            // We are running under the test runner, we may be able to work out the build directory that
+            // contains the test program which is testing libraries in the project. That build directory
+            // should also contain any executables associated with the project until this build type
+            // (eg: release or debug)
+            let loadedTestBundles = Bundle.allBundles.filter({ $0.isLoaded }).filter({ $0.bundlePath.hasSuffix(".xctest") })
+
+            if loadedTestBundles.count > 0 {
+                return loadedTestBundles[0].bundleURL.appendingPathComponent("..")
+            }
+            else {
+                return actualExecutableFolderURL
+            }
+        }
+    #endif
+    }().standardized
 
 /// Absolute path to the executable's folder
-let executableFolder = executableFolderURL().path
+let executableFolder = executableFolderURL.path
 
 /// Directory containing the Package.swift of the project (as determined by traversing
 /// up the directory structure starting at the directory containing the executable), or
 /// if no Package.swift is found then the directory containing the executable
-func findProjectRoot() -> String {
+let projectDirectory = { () -> String in
     let fileManager = FileManager()
-    var directory = executableFolderURL().appendingPathComponent("dummy")
+    var directory = executableFolderURL.appendingPathComponent("dummy")
+
     repeat {
         directory.appendPathComponent("..")
         directory.standardize()
         let packageFilePath = directory.appendingPathComponent("Package.swift").path
+
         if fileManager.fileExists(atPath: packageFilePath) {
             return directory.path
         }
     } while directory.path != "/"
-    return executableFolder
-}
 
-/// Absolute path to the project directory
-let projectDirectory = findProjectRoot()
+    return executableFolder
+}()
 
 /// Absolute path to the present working directory (PWD)
 let presentWorkingDirectory = URL(fileURLWithPath: "").path
